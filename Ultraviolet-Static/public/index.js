6b86551fedd7232a4e97e1dd0dbc194fbea7333b
rogo! <r480github@gmail.com>
2024-10-31 01:35:39 -0400

    I'm mentally insane now

--- Ultraviolet-Static/public/index.js
+++ Ultraviolet-Static/public/index.js
@@ -1,16 +1,7 @@
-function opencloak() {
-	var win = window.open()   
-	var url = "index.html" 
-	var iframe = win.document.createElement('iframe')
-	iframe.style.width = "100%";
-	iframe.style.height = "100%";
-	iframe.src = url;
-	win.document.body.style.margin = "0"; 
-	win.document.body.style.padding = "0"; 
-	win.document.body.appendChild(iframe);
-
- }
- 
+"use strict";
+/**
+ * @type {HTMLFormElement}
+ */
 const form = document.getElementById("uv-form");
 /**
  * @type {HTMLInputElement}
