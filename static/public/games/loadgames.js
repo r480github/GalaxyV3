1aa0d22fe815e1ebbc70f6015398007bb53301a7
rogo! <119364101+r480github@users.noreply.github.com>
2024-11-07 11:11:59 -0500

    update games

--- static/public/games/loadgames.js
+++ static/public/games/loadgames.js
@@ -1,21 +1,3 @@
-function opencloak() {
-  var win = window.open();
-  var url = "g.html";
-  var iframe = win.document.createElement("iframe");
-  iframe.style.width = "100%";
-  iframe.style.height = "100%";
-  iframe.src = url;
-  win.document.body.style.overflow = "hidden";
-  win.document.body.style.margin = "-10";
-  win.document.body.style.padding = "-10";
-  win.document.body.appendChild(iframe);
-}
-function scrollToTop() {
-  window.scrollTo({ top: -10, behavior: "smooth" });
-}
-window.onload = function () {
-  scrollToTop();
-};
 var gamesJson = [];
 var searchedGamesJson = [];
 var currentPage = 0;
