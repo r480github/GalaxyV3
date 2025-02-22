79782402e66d5e71b0d6d0e0b3479dad0c391f6c
rogo! <r480github@gmail.com>
2025-02-22 00:14:50 -0500

    add game files to website for faster loading speeds

--- public/assets/js/games.js
+++ public/assets/js/games.js
@@ -3,52 +3,44 @@ fetch("/assets/json/games.json")
   .then((games) => {
     const appsContainer = document.querySelector(".games");
 
-    games.forEach((game) => {
+    games.forEach((game, index) => {
       const gameElement = document.createElement("div");
       gameElement.className = "game";
+      gameElement.dataset.index = index; 
 
       gameElement.innerHTML = `
-                <img src="/img/${game.image}" alt="${game.name}" class="cards" >
-                        <h3>${game.name}</h3>
-
+        <img src="/img/${game.image}" alt="${game.name}" class="cards">
+        <h3>${game.name}</h3>
       `;
 
       gameElement.addEventListener("click", async () => {
-        var ute = game.url;
-        if (localStorage.getItem("proxy") == "uv") {
-          ute = __uv$config.prefix + __uv$config.encodeUrl(ute);
-          localStorage.setItem("url", ute);
-          window.location.href = "/search.html";
-        } else if (localStorage.getItem("proxy") == "sj") {
-          sjEncode();
-        } else if (localStorage.getItem("proxy") == "rammerhead") {
-          rhEncode();
-        }
-
-        async function rhEncode() {
-          ute = await RammerheadEncode(ute);
-          window.location.href = "/" + ute;
-        }
-        async function sjEncode() {
-          url = "/scram/service/" + encodeURIComponent(ute);
-          localStorage.setItem("url", ute);
-          window.location.href = "/search.html";
+        if (game.url) {
+          var ute = game.url;
+          if (localStorage.getItem("proxy") == "uv") {
+            ute = __uv$config.prefix + __uv$config.encodeUrl(ute);
+            localStorage.setItem("url", ute);
+            window.location.href = "/search.html";
+          } 
+        } else if (game.file) {
+          var fil = game.file;
+          localStorage.setItem('gameload', fil);
+          window.location.href = "/gameload.html";
         }
       });
 
       appsContainer.appendChild(gameElement);
     });
   });
-  document.getElementById('search').addEventListener('input', function() {
-    const searchitem = this.value.toLowerCase();
-    const games = document.querySelectorAll('.game');
-  
-    games.forEach(game => {
-      const gameName = game.querySelector('h3').textContent.toLowerCase();
-      if (gameName.includes(searchitem)) {
-        game.style.display = 'flex';
-      } else {
-        game.style.display = 'none';
-      }
-    });
+
+document.getElementById("search").addEventListener("input", function () {
+  const searchitem = this.value.toLowerCase();
+  const games = document.querySelectorAll(".game");
+  games.forEach((game) => {
+    const gameName = game.querySelector("h3").textContent.toLowerCase();
+    if (gameName.includes(searchitem)) {
+      game.style.display = "flex";
+    } else {
+      game.style.display = "none";
+    }
   });
+});
